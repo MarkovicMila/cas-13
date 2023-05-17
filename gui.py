16119673c35c49edb6ba5164a33bcc7bb8ff8e88
MarkovicMila <127663879+MarkovicMila@users.noreply.github.com>
2023-05-17 16:31:24 +0200

    Sa casa

--- gui.py
+++ gui.py
@@ -1,66 +1,85 @@
-from tkinter import *
 from klasa import *
-
-
+from tkinter import *
 root=Tk()
-root.title("Posiljke")
-def dodaj_posiljku():
+root.title('Posta')
+def dodaj_osobu():
     t=Toplevel(root)
-
-    Label(t,text="Posiljalac").grid(row=0,column=0)
-    Label(t,text="Primalac").grid(row=1,column=0)
-   
+    Label(t,text='Posiljalac').grid(row=0,column=0)
+    Label(t,text='Primalac').grid(row=1,column=0)
+    
     e1_t=Entry(t)
     e1_t.grid(row=0,column=1,columnspan=2)
     e2_t=Entry(t)
     e2_t.grid(row=1,column=1,columnspan=2)
     
-    b_t=Button(t,text="Dodaj posiljku",command=lambda:p.dodaj_posiljku(e1_t.get(),e2_t.get()))
-    b_t.grid(row=2,column=0,columnspan=2)
+
+    b_t=Button(t,text='Dodaj',command=lambda:P.dodaj_posiljku(e1_t.get(),e2_t.get()))
+    b_t.grid(row=2,column=0,columnspan=3)
+
+def export():
+    t=Toplevel(root)
+    button_excel=Button(t,text='Excel',command=lambda:P.export_excel())
+    button_excel.grid(row=0,column=1)
+
+    button_csv=Button(t,text='CSV',command=lambda:P.export_csv())
+    button_csv.grid(row=0,column=2)
 
 def promeni_status():
     t=Toplevel(root)
-    
-    var1 = StringVar(t) 
-    var1.set("poslato") 
-    r1=Radiobutton(t, text="poslato", variable=var1, value="poslato")
-    r2=Radiobutton(t, text="otpremljeno", variable=var1, value="otpremljeno")
-    r3=Radiobutton(t, text="dostavljeno", variable=var1, value="dostavljeno")
 
-    r1.grid(row=0,column=0)
-    r2.grid(row=1,column=0)
-    r3.grid(row=2,column=0)
+    var2=StringVar(t)
+    var2.set('poslato')
+    r1=Radiobutton(t,text='poslato',variable=var2,value='poslato')
+    r2=Radiobutton(t,text='na isporuci',variable=var2,value='na isporuci')
+    r3=Radiobutton(t,text='primljeno',variable=var2,value='primljeno')
+    r1.grid(row=4,column=1)
+    r2.grid(row=4,column=2)
+    r3.grid(row=4,column=3)
 
-    b_e=Button(t,text="Promeni status",command=lambda:p.promeni_status(var1.get()))
-    b_e.grid(row=3,column=0)
-    
-def export():
-    t=Toplevel(root)
-    
-    b_t1=Button(t,text="Excel",command=lambda:p.export_excel())
-    b_t1.grid(row=0,column=1)
-    
-    b_t2=Button(t,text="CSV",command=lambda:p.export_csv())
-    b_t2.grid(row=0,column=2)
+# def export_table(upit,naziv):
+#     L.kreiraj_upit(upit)
+#     L.get_sql()
+#     L.export_excel(naziv)
 
-# list=p.listbox()
-# list=pd.DataFrame(list)
+button_export=Button(root,text='Export',command=lambda:export())
+button_export.grid(row=0,column=2,rowspan=2)
+
+button_dodaj=Button(root,text='Dodaj',command=lambda:dodaj_osobu())
+button_dodaj.grid(row=2,column=2,rowspan=2)
+
+button_promeni_status=Button(root,text='Promeni status',command=lambda:promeni_status())
+button_promeni_status.grid(row=4,column=2,rowspan=2)
+
 listbox=Listbox(root)
-# for i in range(len(list)):
-#     listbox.insert(i+1,list.loc[0:len(list),('broj_posiljke','posiljalac','primalac','status')])
-#     i+=1
-listbox.grid(row=0,column=0,rowspan=9) 
+listbox.grid(row=0,column=1,rowspan=6)
+
+# menubar=Menu(root)
+# dodaj_menu=Menu(menubar,tearoff=0)
+# dodaj_menu.add_command(label='Dodaj coveka',command=lambda:dodaj_osobu())
+# menubar.add_cascade(label='Dodaj',menu=dodaj_menu)
+
+# m="SELECT * FROM LJUDI WHERE POL='M' "
+# b_m=Button(root,text='Export M',height=3,command=lambda:export_table(m,'MUSKARCI'))
+# b_m.grid(row=0,column=0,rowspan=2)
+
+# z="SELECT * FROM LJUDI WHERE POL='Z' "
+# b_z=Button(root,text='Export Z',height=3,command=lambda:export_table(z,'ZENE'))
+# b_z.grid(row=3,column=0,rowspan=2)
 
 
+# var1=StringVar()
+# var1.set('ORDER BY JMBG ASC')
+# r1=Radiobutton(root,text='Ime',variable=var1,value='ORDER BY IME ASC')
+# r2=Radiobutton(root,text='Prezime',variable=var1,value='ORDER BY PREZIME ASC')
+# r3=Radiobutton(root,text='Godine',variable=var1,value='ORDER BY GODINE ASC')
 
-b_dodaj=Button(root,text="Dodaj posiljku",height=3,command=lambda:dodaj_posiljku())
-b_dodaj.grid(row=0,column=1,rowspan=3)
+# r1.grid(row=0,column=1)
+# r2.grid(row=1,column=1)
+# r3.grid(row=2,column=1)
 
-b_promeni=Button(root,text="Promeni status",height=3,command=lambda:promeni_status())
-b_promeni.grid(row=3,column=1,rowspan=3)
+# b_e=Button(root,text="Export",command=lambda:export_table("SELECT * FROM LJUDI {}".format(var1.get()),"Ljudi_Sort"))
+# b_e.grid(row=3,column=1)
 
-b_export=Button(root,text="Export",height=3,command=lambda:export())
-b_export.grid(row=6,column=1,rowspan=3)
-
+# root.config(menu=menubar)
 
 mainloop()
--- klasa.py
+++ klasa.py
@@ -1,102 +1,58 @@
 import psycopg2 as pg
-import openpyxl as op
 import pandas as pd
 
-class Posiljke:
+class Posta:
     def __init__(self):
-        pass
-    def dodaj_posiljku(self,posiljalac,primalac):
-        try:
-            con=pg.connect(
+        self.upit=""
+        self.posiljke=None
+
+    def kreiraj_upit(self,upit):
+        self.upit=upit
+    
+    def get_sql(self):
+        con=pg.connect(
                 database='POSTA',
+                user='postgres',
                 port='5432',
                 host='localhost',
-                user='postgres',
                 password='itoip'
-            )
-            df=pd.read_sql('SELECT * FROM POSILJKE',con=con)
-            cursor=con.cursor()
-            l='''INSERT INTO POSILJKE VALUES ({},'{}','{}','poslato');'''.format(len(df)+1,posiljalac,primalac)
-            cursor.execute(l)
-            con.commit()
-        except(Exception,pg.Error) as e:
-            print('Error: ',e)
-        finally:
-            con.close()
-            cursor.close()
+        )
+        self.posiljke=pd.read_sql('SELECT * FROM POSILJKE',con)
+        con.close()
+        return 'Dataframe created'
+        
     def export_excel(self):
-        try:
-            con=pg.connect(
-                database='POSTA',
-                port='5432',
-                host='localhost',
-                user='postgres',
-                password='itoip'
-            )
-            df=pd.read_sql('SELECT * FROM POSILJKE',con=con)
-            df.to_excel('Excel.xlsx', index=False)
-        except(Exception,pg.Error) as e:
-            print('Error: ',e)
-        finally:
-            con.close()
+        if self.posiljke.size!=0:
+            self.posiljke.to_excel('Posiljke.xlsx',index=False)
+            return 'Excel exported'
+        else:
+            return 'An error ocurred'
     def export_csv(self):
+        if self.posiljke.size!=0:
+            self.posiljke.to_csv('Posiljke.csv',index=False)
+            return 'CSV exported'
+        else:
+            return 'An error ocurred'
+        
+    
+    def dodaj_posiljku(self,posiljalac,primalac,status='poslato'):
         try:
             con=pg.connect(
                 database='POSTA',
-                port='5432',
+                user='postgres',
                 host='localhost',
-                user='postgres',
-                password='itoip'
-            )
-            df=pd.read_sql('SELECT * FROM POSILJKE',con=con)
-            df.to_csv('CSV.csv', index=False)
-        except(Exception,pg.Error) as e:
-            print('Error: ',e)
-        finally:
-            con.close()
-    def promeni_status(self,status):
-        try:
-            con=pg.connect(
-                database='POSTA',
-                user='postgres',
                 port='5432',
-                password='itoip',
-                host='localhost'
+                password='itoip'
             )
             cursor=con.cursor()
-
-            com='''
-            UPDATE POSILJKE
-            SET STATUS='{}'
-            '''.format(status)
+            com='''INSERT INTO POSILJKE (POSILJALAC,PRIMALAC,STATUS)VALUES ('{}','{}','{}');'''.format(posiljalac,primalac,status)
             cursor.execute(com)
-            print("Table updated successfully")
             con.commit()
-
-        except (Exception,pg.Error) as e:
-            print("Error:",e)
-
+        except(Exception,pg.Error) as e:
+            print('Error: ',e)
         finally:
-            cursor.close()
             con.close()
-    # def listbox(self):
-    #     try:
-    #         con=pg.connect(
-    #             database='POSTA',
-    #             port='5432',
-    #             host='localhost',
-    #             user='postgres',
-    #             password='itoip'
-    #         )
-    #         df=pd.read_sql('SELECT * FROM POSILJKE',con=con)
-    #         return df
-    #     except(Exception,pg.Error) as e:
-    #         print('Error: ',e)
-    #     finally:
-    #         con.close()
+            cursor.close()
+P=Posta()
+P.get_sql()
 
-p=Posiljke()
-# p.export_excel()
-# p.export_csv()
-# p.dodaj_posiljku('Petar Petrovic','Marko Markovic')
-# print(p.listbox())
